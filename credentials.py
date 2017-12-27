1095dcd37b77561999c216a214e93ab793285641
Job Visser <jvisser@jibes.nl>
2017-12-27 14:31:22 +0100

    put logic in src and separate streamer script for reddit.

---
+++ credentials.py
@@ -0,0 +1,13 @@
+# twitter credentials
+twitter_consumer_key = 'rIe6nlKs7NqlJlmUdXMe5Hei3'
+twitter_consumer_secret = 'Q4nrJ5mkViw9e3pp91y8GYmGTdd372yIvba2V3iNJAIoTLGR2s'
+twitter_access_token = '118662390-X9inf3rdbb9MzH6ZK9v8cus3KQ001m1NXq1QmbxA'
+twitter_access_token_secret = 'VtMLjQCE5TFqEkvxYbdJLYMA0lQasVOkIeis3E4VwQGvW'
+
+# reddit credentials
+reddit_id = '7Z3kvicoZrQG4w'
+reddit_secret = 'FmKJUKwwVkEBCIyUHqlSBif87BU'
+reddit_app_name = 'crypto_news_bot'
+
+# slack
+slack_bot_token = 'xoxb-289925568819-4KGqZzmrDz0puWOIqbNjIr4i'
---
+++ src.py
@@ -0,0 +1,69 @@
+import praw
+from slackclient import SlackClient
+import tweepy
+import credentials
+
+
+def twitter_login():
+    auth = tweepy.OAuthHandler(credentials.twitter_consumer_key,
+                               credentials.twitter_consumer_secret)
+    auth.set_access_token(credentials.twitter_access_token,
+                          credentials.twitter_access_token_secret)
+    t = tweepy.API(auth)
+    return t
+
+
+def slack_login():
+    # instantiate Slack client
+    s = SlackClient(credentials.slack_bot_token)
+    # starterbot's user ID in Slack: value is assigned after the bot starts up
+    # starterbot_id = None
+    return s
+
+
+def reddit_login():
+    r = praw.Reddit(
+        client_id=credentials.reddit_id,
+        client_secret=credentials.reddit_secret,
+        user_agent='crypto_news_bot:v0.1 (by /u/Kapsalon21)')
+    return r
+
+
+def process_priority(item):
+
+    # check title for coin mentions
+    coins = {'BTC': 'Bitcoin',
+             'LTC': 'Litecoin',
+             'XRP': 'Ripple',
+             'FCT': 'Factom'}
+
+    # exclude when
+    block_words = ['?', 'Who', 'Why', 'hype']
+
+    # include when
+    prio_words = ['pump', 'blow', 'bull', 'grow']
+
+    has_prio = any(string in item for string in prio_words)
+    print("Contains priority words: {}".format(has_prio))
+    has_coins = any(string in item for string in list(coins.keys()))
+    print("Contains coins: {}".format(has_coins))
+    has_block = any(string in item for string in block_words)
+    print("Contains block words: {}".format(has_block))
+
+    if has_prio and has_coins and not has_block:
+        return True
+    else:
+        return False
+
+
+def post_to_slack(s, item, channel):
+    s.api_call(
+        "chat.postMessage",
+        channel=channel,
+        text=item
+    )
+    return True
+
+
+# constants
+RTM_READ_DELAY = 1 # 1 second delay between reading from RTM
